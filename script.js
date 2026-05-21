8c92c8e34231a9a6dd6ffa5724725a2b519cae64
Soman21 <maryannmmesoma@gmail.com>
2026-05-21 16:09:38 +0100

    feat: cart system

--- script.js
+++ script.js
@@ -1,3 +1,50 @@
+const cartCount = document.getElementById("cart-count");
+
+let cart = [];
+
+cart = [];
+
+// const buttons = document.querySelectorAll(".add-to-cart-btn");
+
+// buttons.forEach((button) => {
+   
+//     button.addEventListener("click", () => {
+
+//         const productId = Number(button.dataset.id);
+
+//         addToCart(productId);
+//     });
+// });
+document.addEventListener("click", function (e) {
+    if (e.target.classList.contains("add-to-cart-btn")) {
+
+        const productId = Number(e.target.dataset.id);
+
+        addToCart(productId);
+        }
+});
+
+
+function addToCart(productId) {
+    console.log("clicked");
+
+    const product = products.find(item => item.id === productId);
+    
+    cart.push(product);
+    cartCount.textContent = cart.length;
+}
+
+
+
+
+
+
+
+
+
+
+
+
 const products = [
   {
     id: 1,
@@ -8,9 +55,9 @@ const products = [
 
   {
     id: 2,
-    name: "Flow Tank Top",
+    name: "Leather Brown Bag",
     price: 80,
-    image: "https://images.unsplash.com/photo-1483985988355-763728e1935b?q=80&w=800"
+    image: "https://images.unsplash.com/photo-1612902456551-333ac5afa26e?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
 
   {
@@ -18,6 +65,13 @@ const products = [
     name: "Studio Jacket",
     price: 150,
     image: "https://images.unsplash.com/photo-1529139574466-a303027c1d8b?q=80&w=800"
+  }, 
+
+  {
+    id: 4,
+    name: "Fur Coat Jacket",
+    price: 250,
+    image: "https://plus.unsplash.com/premium_photo-1673757089859-b668780f5790?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   }
 ];
 
@@ -25,33 +79,35 @@ const productGrid = document.getElementById("product-grid");
 
 products.forEach((product) => {
 
-  productGrid.innerHTML += `
-  
-        <div class="bg-white p-3 rounded-xl shadow-sm flex flex-col">
+productGrid.innerHTML += `
+  <div class="bg-white rounded-xl shadow-sm overflow-hidden">
 
-            <img 
-                src="${product.image}" 
-                alt="${product.name}"
-                class="w-full h-52 object-cover rounded-lg"
-            />
+    <!-- IMAGE -->
+    <div class="rounded-lg overflow-hidden" style="width: 100%; height: 320px; background: #f3f4f6;">
+      <img 
+        src="${product.image}" 
+        alt="${product.name}"
+        style="width: 100%; height: 100%; object-fit: cover; object-position: center; display: block;"
+      />
+    </div>
 
-            <div class="flex flex-col flex-1 mt-3">
+    <!-- CONTENT -->
+    <div class="p-4">
+      <h3 class="text-sm font-medium text-gray-900">
+        ${product.name}
+      </h3>
 
-                <h3 class="font-medium text-sm">
-                ${product.name}
-                </h3>
+      <p class="text-gray-500 text-sm mt-2">
+        $${product.price}
+      </p>
 
-                <p class="text-sm text-gray-500 mt-1">
-                $${product.price}
-                </p>
+      <button 
+      class="add-to-cart-btn mt-3 w-full bg-black text-white py-2 rounded-lg text-sm hover:bg-gray-800"
+      data-id="${product.id}">
+        Add to Cart
+      </button>
+    </div>
 
-                <button class="mt-auto w-full bg-black text-white py-2 rounded-lg">
-                Add to Cart
-                </button>
-
-            </div>
-
-        </div>
-
-  `;
+  </div>
+`;
 });
